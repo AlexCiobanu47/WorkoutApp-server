0136edac800f2a0d824e3ae6f94d4bb3350f63cc
AlexCiobanu <alexciobanu72@yahoo.com>
2023-04-23 13:42:41 +0300

    added routes for the api

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
+//express app
+const app = express();
+
+//middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+//routes
+app.use("/api/v1/workouts", workoutRoutes);
+app.use(workoutRoutes);
+//listen for requests
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port 4000");
+});
